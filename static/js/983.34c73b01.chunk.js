9af96c54b49fe2ea8b66c73deb39b52b783fb303
alexanderelmuratov <85258068+alexanderelmuratov@users.noreply.github.com>
2022-03-17 20:56:47 +0000

    Deploying to gh-pages from @ alexanderelmuratov/goit-react-hw-08-phonebook@5540b317f5b8258dc99d0c1b0aebc7b0b6edea4e 🚀

---
+++ static/js/983.34c73b01.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,a){a.r(t),a.d(t,{HomePage:function(){return o}});var r=a(4169),n=a(184),o=function(){return(0,n.jsxs)("main",{children:[(0,n.jsx)(r.Dx,{children:"Welcome to Phonebook"}),(0,n.jsx)(r.DK,{children:"Create your personal contact database with a handy search feature!"})]})}}}]);
+//# sourceMappingURL=983.34c73b01.chunk.js.map
